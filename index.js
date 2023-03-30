a7d176c7d531b527e99e9e1ccf30bfa83b4ff15a
RobertJ <robet.jakowenko@nordcloud.com>
2023-03-30 16:54:54 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import { createServer } from "http";
+
+const server = createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("<h1>Hello, World</h1>");
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
